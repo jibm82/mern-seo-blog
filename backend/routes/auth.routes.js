a2666f68a28e132064177298c697b93471245ff6
Jorge Báez <jibm_82@yahoo.com.mx>
2022-03-12 16:26:47 -0600

    Implement user validation

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,6 +2,10 @@ const express = require("express")
 const router = express.Router()
 const { signUp } = require("../controllers/auth.controller")
 
-router.get("/sign-up", signUp)
+// Validators
+const { runValidation } = require("../validators")
+const { userSignUpValidator } = require("../validators/auth.validator")
+
+router.post("/sign-up", userSignUpValidator, runValidation, signUp)
 
 module.exports = router
