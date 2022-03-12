f7c7e170babcf152c9f31967cac50ffebfa4969d
Jorge Báez <jibm_82@yahoo.com.mx>
2022-03-12 16:52:14 -0600

    Implement user sign up

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,36 @@
+const User = require("../models/user.model")
+const shortId = require("shortid")
+
 exports.signUp = (req, res) => {
   const { name, email, password } = req.body
 
-  res.json({
-    user: {
-      name, email, password, hola: 1,
+  User.findOne({ email }).exec((_err, user) => {
+    if(user) {
+      return res.status(400).json({
+        error: "Email is taken"
+      })
     }
+
+    const username = shortId.generate()
+
+    const profile = `${process.env.CLIENT_URL}/profile/${username}`
+
+    const newUser = new User({ name, email, password, profile, username})
+
+    newUser.save((err, success) => {
+      if(err) {
+        return res.status(400).json({
+          error: err
+        })
+      }
+
+      res.json({
+        user: success
+      })
+
+      // res.json({
+      //   message: "Sign up success! Please sign in"
+      // })
+    })
   })
 }
